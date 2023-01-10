1dc785eeeeb9881e5faa329cce9304724f23f92a
Rafael Oliveira Souza <71654681+rafaoliveira22@users.noreply.github.com>
2023-01-10 14:41:41 -0300

    update code

---
+++ exercicios-logica-github/list/ex092_LIST.py
@@ -0,0 +1,9 @@
+"""
+Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+"""
+list_ints = []
+while len(list_ints) < 5:
+    num_int = int(input('Digite um número inteiro: '))
+    list_ints.append(num_int)
+
+print(list_ints)
